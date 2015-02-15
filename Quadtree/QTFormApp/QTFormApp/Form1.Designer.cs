6fe4611ff34ca1d82d0d5c2249d586c40e7cd874
Michael Gordon <neologue@gmail.com>
2015-02-14 22:26:13 -0500

    Added functional 'Draw white/black node' menu options

--- Quadtree/QTFormApp/QTFormApp/Form1.Designer.cs
+++ Quadtree/QTFormApp/QTFormApp/Form1.Designer.cs
@@ -30,17 +30,26 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.opentoComeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.savetoComeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveAstoComeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.drawToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.drawWhiteNodeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.drawBlackNodeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.drawGreyNodetoComeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.drawRandomNodetoComeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.canvas = new System.Windows.Forms.Panel();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem});
+            this.fileToolStripMenuItem,
+            this.drawToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(284, 24);
@@ -57,13 +66,6 @@
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // opentoComeToolStripMenuItem
             // 
@@ -83,17 +85,83 @@
             this.saveAstoComeToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
             this.saveAstoComeToolStripMenuItem.Text = "Save as <to come>";
             // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // drawToolStripMenuItem
+            // 
+            this.drawToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.drawWhiteNodeToolStripMenuItem,
+            this.drawBlackNodeToolStripMenuItem,
+            this.drawGreyNodetoComeToolStripMenuItem,
+            this.drawRandomNodetoComeToolStripMenuItem});
+            this.drawToolStripMenuItem.Name = "drawToolStripMenuItem";
+            this.drawToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.drawToolStripMenuItem.Text = "Draw";
+            // 
+            // drawWhiteNodeToolStripMenuItem
+            // 
+            this.drawWhiteNodeToolStripMenuItem.Name = "drawWhiteNodeToolStripMenuItem";
+            this.drawWhiteNodeToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.drawWhiteNodeToolStripMenuItem.Text = "Draw white node";
+            this.drawWhiteNodeToolStripMenuItem.Click += new System.EventHandler(this.drawWhiteNodeToolStripMenuItem_Click);
+            // 
+            // drawBlackNodeToolStripMenuItem
+            // 
+            this.drawBlackNodeToolStripMenuItem.Name = "drawBlackNodeToolStripMenuItem";
+            this.drawBlackNodeToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.drawBlackNodeToolStripMenuItem.Text = "Draw black node";
+            this.drawBlackNodeToolStripMenuItem.Click += new System.EventHandler(this.drawBlackNodeToolStripMenuItem_Click);
+            // 
+            // drawGreyNodetoComeToolStripMenuItem
+            // 
+            this.drawGreyNodetoComeToolStripMenuItem.Name = "drawGreyNodetoComeToolStripMenuItem";
+            this.drawGreyNodetoComeToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.drawGreyNodetoComeToolStripMenuItem.Text = "Draw grey node <to come>";
+            // 
+            // drawRandomNodetoComeToolStripMenuItem
+            // 
+            this.drawRandomNodetoComeToolStripMenuItem.Name = "drawRandomNodetoComeToolStripMenuItem";
+            this.drawRandomNodetoComeToolStripMenuItem.Size = new System.Drawing.Size(239, 22);
+            this.drawRandomNodetoComeToolStripMenuItem.Text = "Draw random node <to come>";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(284, 236);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.SystemColors.Window;
+            this.canvas.Location = new System.Drawing.Point(0, 27);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(284, 236);
+            this.canvas.TabIndex = 2;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            this.canvas.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDoubleClick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -107,6 +175,13 @@
         private System.Windows.Forms.ToolStripMenuItem opentoComeToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem savetoComeToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveAstoComeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem drawToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem drawWhiteNodeToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel canvas;
+        private System.Windows.Forms.ToolStripMenuItem drawBlackNodeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem drawGreyNodetoComeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem drawRandomNodetoComeToolStripMenuItem;
     }
 }
 
--- Quadtree/QTFormApp/QTFormApp/Form1.cs
+++ Quadtree/QTFormApp/QTFormApp/Form1.cs
@@ -12,6 +12,9 @@ namespace QTFormApp
 {
     public partial class Form1 : Form
     {
+        private String clicked;
+        public Point coords;
+
         public Form1()
         {
             InitializeComponent();
@@ -21,5 +24,66 @@ namespace QTFormApp
         {
             this.Close();
         }
+
+        private void drawWhiteNodeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            clicked = "drawWhiteNode";
+            MessageBox.Show("Double click anywhere on the panel to draw a white node.");
+        }
+
+        private void drawBlackNodeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            clicked = "drawBlackNode";
+            MessageBox.Show("Double click anywhere on the panel to draw a black node.");
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (e.GetType() == typeof(MouseEventArgs))
+            {
+                MouseEventArgs mea = (MouseEventArgs)e;
+                coords = mea.Location;
+            }
+        }
+
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            //Graphics graphic_obj = canvas.CreateGraphics();
+            //Brush black = new SolidBrush(Color.Black);
+            //Pen border = new Pen(black, 1);
+            //switch (clicked)
+            //{
+            //    case "drawNode":
+            //        graphic_obj.DrawEllipse(border, new Rectangle(new Point(50, 50), new Size(50, 25)));
+            //        break;
+
+            //    default:
+            //        break;
+            //}
+        }
+
+        private void canvas_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            MouseEventArgs mea = e;
+            Point whereClicked = mea.Location;
+         
+            Graphics graphic_obj = canvas.CreateGraphics();
+            Brush black = new SolidBrush(Color.Black);
+            Pen border = new Pen(black, 1);
+            switch (clicked)
+            {
+                case "drawWhiteNode":
+                    graphic_obj.DrawEllipse(border, new Rectangle(whereClicked, new Size(50, 25)));
+                    break;
+                case "drawBlackNode":
+                    graphic_obj.FillEllipse(black, new Rectangle(whereClicked, new Size(50, 25)));
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
     }
 }
