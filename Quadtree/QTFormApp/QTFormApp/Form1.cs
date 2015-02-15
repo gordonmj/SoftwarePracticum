ce4865b6a1d27d34baff41fdc814b07b159e46b3
Michael Gordon <neologue@gmail.com>
2015-02-14 21:07:06 -0500

    Added File dropdown and functional Exit button

--- Quadtree/QTFormApp/QTFormApp/Form1.cs
+++ Quadtree/QTFormApp/QTFormApp/Form1.cs
@@ -16,5 +16,10 @@ namespace QTFormApp
         {
             InitializeComponent();
         }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
